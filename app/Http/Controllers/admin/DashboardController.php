899ab22ed2e0eb136dbdd4dd764a915b3a0c5b1e
fajarhidayatt <fadjhar10@gmail.com>
2023-12-14 20:20:17 +0700

    refactor: admin dashboard

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -3,14 +3,23 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Member;
+use App\Models\Outlet;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $outletTotal = Outlet::all()->count();
+        $memberTotal = Member::all()->count();
+        $transactionTotal = Transaction::all()->count();
+
         return view('admin.index', [
-            'title' => 'dashboard'
+            'outletTotal' => $outletTotal,
+            'memberTotal' => $memberTotal,
+            'transactionTotal' => $transactionTotal,
         ]);
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,7 +1,6 @@
 @extends('admin.partials.main')
 
 @section('content')
-
 <div class="container-fluid">
     <div class="row bg-title">
         <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
@@ -18,7 +17,7 @@
                     </li>
                     <li class="text-right">
                         <i class="ti-arrow-up text-success"></i>
-                        <span class="counter text-success">12</span>
+                        <span class="counter text-success">{{ $outletTotal }}</span>
                     </li>
                 </ul>
             </div>
@@ -32,7 +31,7 @@
                     </li>
                     <li class="text-right">
                         <i class="ti-arrow-up text-purple"></i>
-                        <span class="counter text-purple">12</span>
+                        <span class="counter text-purple">{{ $memberTotal }}</span>
                     </li>
                 </ul>
             </div>
@@ -46,47 +45,11 @@
                     </li>
                     <li class="text-right">
                         <i class="ti-arrow-up text-info"></i>
-                        <span class="counter text-info">12</span>
+                        <span class="counter text-info">{{ $transactionTotal }}</span>
                     </li>
                 </ul>
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-12 col-lg-12 col-sm-12">
-            <div class="white-box">
-                <h3 class="box-title">10 Transaksi Terbaru</h3>
-                <div class="table-responsive">
-                    <table class="table ">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Invoice</th>
-                                <th>Member</th>
-                                <th>Status</th>
-                                <th>Pembayaran</th>
-                                <th>Total Harga</th>
-                                <th width="15%">Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>kode_invoice</td>
-                                <td>nama_member</td>
-                                <td>status</td>
-                                <td>status_bayar</td>
-                                <td>total_harga</td>
-                                <td align="center">
-                                    <a href="#" data-toggle="tooltip" data-placement="bottom" title="Edit" class="btn btn-success btn-block">Detail</a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
-
 @endsection
